69749a803bdd0102e69a58b3b3ad7d67f9774e81
lakleeman <lakleeman@hotmail.com>
2026-08-05 21:12:19 -0500

    1

--- docutest/docusaurus/my-website/docusaurus.config.js
+++ docutest/docusaurus/my-website/docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lakleeman/docutest/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
