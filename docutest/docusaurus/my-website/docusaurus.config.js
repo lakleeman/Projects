33145686e5a65b8d3710f6864ad09d21fce85a51
lakleeman <lakleeman@hotmail.com>
2026-08-05 21:24:01 -0500

    2

--- docutest/docusaurus/my-website/docusaurus.config.js
+++ docutest/docusaurus/my-website/docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lakleeman/Projects/tree/master/docutest/docusaurus/my-website',
         },
         blog: {
           showReadingTime: true,
@@ -61,7 +61,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/lakleeman/docutest/',
+            'https://github.com/lakleeman/Projects/tree/master/docutest/docusaurus/my-website',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
