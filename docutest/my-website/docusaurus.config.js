9715dcb225d5b54c063c5b6f932e22bae7d5318f
lakleeman <lakleeman@hotmail.com>
2026-08-07 18:25:38 -0500

    fixed errors

--- docutest/my-website/docusaurus.config.js
+++ docutest/my-website/docusaurus.config.js
@@ -125,7 +125,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/current/intro',
               },
             ],
           },
