dd413f283b6be576571b6dba9fe4b37f3ef16282
lakleeman <lakleeman@hotmail.com>
2026-08-07 19:02:58 -0500

    corrected baseurl

--- docutest/my-website/docusaurus.config.js
+++ docutest/my-website/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://larrykleeman.com',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // For a subpath deployment such as /docutest/, use '/docutest/'
+  baseUrl: '/docutest/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
